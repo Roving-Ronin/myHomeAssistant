e10282c18f0d79e27193ddcbe4d6d7beaa47c55f
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-07-08 19:38:42 +1000

    Create modbus_proxy.h

---
+++ custom_components/modbus_proxy.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "esphome.h"
+
+class ModbusProxy : public Component {
+ public:
+  void setup() override {
+    // Initialize the Modbus proxy here
+  }
+  
+  void loop() override {
+    // Main loop for the Modbus proxy
+  }
+};
