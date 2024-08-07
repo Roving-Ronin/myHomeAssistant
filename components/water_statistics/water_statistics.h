2194f7329679f8853b05f68d804545cb767a0454
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-08-07 11:56:36 +1000

    Update water_statistics.h

--- components/water_statistics/water_statistics.h
+++ components/water_statistics/water_statistics.h
@@ -7,11 +7,11 @@
 #include "esphome/components/time/real_time_clock.h"
 
 namespace esphome {
-namespace energy_statistics {
+namespace water_statistics {
 
 using sensor::Sensor;
 
-class EnergyStatistics : public Component {
+class WaterStatistics : public Component {
  public:
   float get_setup_priority() const override { return setup_priority::DATA; }
   void dump_config() override;
@@ -21,10 +21,10 @@ class EnergyStatistics : public Component {
   void set_time(time::RealTimeClock *time) { this->time_ = time; }
   void set_total(Sensor *sensor) { this->total_ = sensor; }
 
-  void set_energy_today(Sensor *sensor) { this->energy_today_ = sensor; }
-  void set_energy_yesterday(Sensor *sensor) { this->energy_yesterday_ = sensor; }
-  void set_energy_week(Sensor *sensor) { this->energy_week_ = sensor; }
-  void set_energy_month(Sensor *sensor) { this->energy_month_ = sensor; }
+  void set_water_today(Sensor *sensor) { this->water_today_ = sensor; }
+  void set_water_yesterday(Sensor *sensor) { this->water_yesterday_ = sensor; }
+  void set_water_week(Sensor *sensor) { this->water_week_ = sensor; }
+  void set_water_month(Sensor *sensor) { this->water_month_ = sensor; }
 
  protected:
   ESPPreferenceObject pref_;
@@ -34,27 +34,27 @@ class EnergyStatistics : public Component {
   Sensor *total_{nullptr};
 
   // exposed sensors
-  Sensor *energy_today_{nullptr};
-  Sensor *energy_yesterday_{nullptr};
-  Sensor *energy_week_{nullptr};
-  Sensor *energy_month_{nullptr};
+  Sensor *water_today_{nullptr};
+  Sensor *water_yesterday_{nullptr};
+  Sensor *water_week_{nullptr};
+  Sensor *water_month_{nullptr};
 
   // start day of week configuration
-  int energy_week_start_day_{2};
+  int water_week_start_day_{2};
   // start day of month configuration
-  int energy_month_start_day_{1};
+  int water_month_start_day_{1};
 
-  struct energy_data_t {
+  struct water_data_t {
     uint16_t current_day_of_year{0};
     float start_today{NAN};
     float start_yesterday{NAN};
     float start_week{NAN};
     float start_month{NAN};
-  } energy_;
+  } water_;
 
   void process_(float total);
   void save_();
-};  // class EnergyStatistics
+};  // class WaterStatistics
 
-}  // namespace energy_statistics
+}  // namespace water_statistics
 }  // namespace esphome
