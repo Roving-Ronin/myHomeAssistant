461fdd992231a6ecae7afccac0dc5761fb460b5b
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-08-07 12:19:15 +1000

    Add custom component for ESPHome

---
+++ esphome/components/custom_components/my_custom_component.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "esphome.h"
+
+class MyCustomComponent : public Component {
+  public:
+    void setup() override {
+      // Custom setup code here
+    }
+    void loop() override {
+      // Custom loop code here
+    }
+};
