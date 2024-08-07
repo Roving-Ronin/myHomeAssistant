49cecc10824f8f3e04e2ae6fe7f0c06e2dc426fa
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-08-07 11:55:00 +1000

    Update sensor.py

--- components/water_statistics/sensor.py
+++ components/water_statistics/sensor.py
@@ -6,7 +6,7 @@ from esphome.const import (
     CONF_TIME_ID,
     CONF_POWER,
     CONF_TOTAL,
-    DEVICE_CLASS_ENERGY,
+    DEVICE_CLASS_WATER,
     STATE_CLASS_TOTAL_INCREASING,
     UNIT_KILOWATT_HOURS,
 )
@@ -15,42 +15,42 @@ CODEOWNERS = ["@dentra"]
 
 DEPENDENCIES = ["time"]
 
-CONF_ENERGY_TODAY = "energy_today"
-CONF_ENERGY_YESTERDAY = "energy_yesterday"
-CONF_ENERGY_WEEK = "energy_week"
-CONF_ENERGY_MONTH = "energy_month"
+CONF_WATER_TODAY = "water_today"
+CONF_water_YESTERDAY = "water_yesterday"
+CONF_WATER_WEEK = "water_week"
+CONF_WATER_MONTH = "water_month"
 
-energy_statistics_ns = cg.esphome_ns.namespace("energy_statistics")
+water_statistics_ns = cg.esphome_ns.namespace("water_statistics")
 
-EnergyStatistics = energy_statistics_ns.class_("EnergyStatistics", cg.Component)
+EnergyStatistics = water_statistics_ns.class_("EnergyStatistics", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(EnergyStatistics),
         cv.GenerateID(CONF_TIME_ID): cv.use_id(time.RealTimeClock),
         cv.Required(CONF_TOTAL): cv.use_id(sensor.Sensor),
-        cv.Optional(CONF_ENERGY_TODAY): sensor.sensor_schema(
+        cv.Optional(CONF_WATER_TODAY): sensor.sensor_schema(
             unit_of_measurement=UNIT_KILOWATT_HOURS,
             accuracy_decimals=2,
-            device_class=DEVICE_CLASS_ENERGY,
+            device_class=DEVICE_CLASS_WATER,
             state_class=STATE_CLASS_TOTAL_INCREASING,
         ),
-        cv.Optional(CONF_ENERGY_YESTERDAY): sensor.sensor_schema(
+        cv.Optional(CONF_WATER_YESTERDAY): sensor.sensor_schema(
             unit_of_measurement=UNIT_KILOWATT_HOURS,
             accuracy_decimals=2,
-            device_class=DEVICE_CLASS_ENERGY,
+            device_class=DEVICE_CLASS_WATER,
             state_class=STATE_CLASS_TOTAL_INCREASING,
         ),
-        cv.Optional(CONF_ENERGY_WEEK): sensor.sensor_schema(
+        cv.Optional(CONF_WATER_WEEK): sensor.sensor_schema(
             unit_of_measurement=UNIT_KILOWATT_HOURS,
             accuracy_decimals=2,
-            device_class=DEVICE_CLASS_ENERGY,
+            device_class=DEVICE_CLASS_WATER,
             state_class=STATE_CLASS_TOTAL_INCREASING,
         ),
-        cv.Optional(CONF_ENERGY_MONTH): sensor.sensor_schema(
+        cv.Optional(CONF_WATER_MONTH): sensor.sensor_schema(
             unit_of_measurement=UNIT_KILOWATT_HOURS,
             accuracy_decimals=2,
-            device_class=DEVICE_CLASS_ENERGY,
+            device_class=DEVICE_CLASS_WATER,
             state_class=STATE_CLASS_TOTAL_INCREASING,
         ),
     }
@@ -88,7 +88,7 @@ async def to_code(config):
     await setup_input(config, CONF_TOTAL, var.set_total)
 
     # exposed sensors
-    await setup_sensor(config, CONF_ENERGY_TODAY, var.set_energy_today)
-    await setup_sensor(config, CONF_ENERGY_YESTERDAY, var.set_energy_yesterday)
-    await setup_sensor(config, CONF_ENERGY_WEEK, var.set_energy_week)
-    await setup_sensor(config, CONF_ENERGY_MONTH, var.set_energy_month)
+    await setup_sensor(config, CONF_WATER_TODAY, var.set_water_today)
+    await setup_sensor(config, CONF_WATER_YESTERDAY, var.set_water_yesterday)
+    await setup_sensor(config, CONF_WATER_WEEK, var.set_water_week)
+    await setup_sensor(config, CONF_WATER_MONTH, var.set_water_month)
