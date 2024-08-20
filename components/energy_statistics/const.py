c5555baa9b69adbe568faf1f5334b9114a2670ae
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-08-20 18:28:07 +1000

    Create const.py

---
+++ components/energy_statistics/const.py
@@ -0,0 +1,8 @@
+# const.py
+UNIT_LITRE = "L"
+
+ICON_TODAY = "mdi:hours-24"
+ICON_YESTERDAY = "mdi:calendar-today-outline"
+ICON_WEEK = "mdi:calendar-week"
+ICON_MONTH = "mdi:calendar"
+ICON_YEAR = "mdi:calendar-text"
