8df5aa1e44f949f64c2e1e15b06dd97cbf4c0f5f
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-08-30 12:44:08 +1000

    Create sensor.py

---
+++ components/gas_statistics_mj/sensor.py
@@ -0,0 +1,102 @@
+import esphome.config_validation as cv
+import esphome.codegen as cg
+from esphome.components import sensor, time
+from esphome.const import (
+    CONF_ID,
+    CONF_TIME_ID,
+    CONF_POWER,
+    CONF_TOTAL,
+    DEVICE_CLASS_GAS,
+    STATE_CLASS_TOTAL_INCREASING,
+    UNIT_CUBIC_METER,
+)
+
+CODEOWNERS = ["@dentra", "@roving-ronin"]
+
+DEPENDENCIES = ["time"]
+
+CONF_GAS_TODAY = "gas_today"
+CONF_GAS_YESTERDAY = "gas_yesterday"
+CONF_GAS_WEEK = "gas_week"
+CONF_GAS_MONTH = "gas_month"
+CONF_GAS_YEAR = "gas_year"
+
+gas_statistics_ns = cg.esphome_ns.namespace("gas_statistics")
+
+GasStatistics = gas_statistics_ns.class_("GasStatistics", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(GasStatistics),
+        cv.GenerateID(CONF_TIME_ID): cv.use_id(time.RealTimeClock),
+        cv.Required(CONF_TOTAL): cv.use_id(sensor.Sensor),
+        cv.Optional(CONF_GAS_TODAY): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CUBIC_METER,
+            accuracy_decimals=3,
+            device_class=DEVICE_CLASS_GAS,
+            state_class=STATE_CLASS_TOTAL_INCREASING,
+        ),
+        cv.Optional(CONF_GAS_YESTERDAY): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CUBIC_METER,
+            accuracy_decimals=3,
+            device_class=DEVICE_CLASS_GAS,
+            state_class=STATE_CLASS_TOTAL_INCREASING,
+        ),
+        cv.Optional(CONF_GAS_WEEK): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CUBIC_METER,
+            accuracy_decimals=3,
+            device_class=DEVICE_CLASS_GAS,
+            state_class=STATE_CLASS_TOTAL_INCREASING,
+        ),
+        cv.Optional(CONF_GAS_MONTH): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CUBIC_METER,
+            accuracy_decimals=3,
+            device_class=DEVICE_CLASS_GAS,
+            state_class=STATE_CLASS_TOTAL_INCREASING,
+        ),
+        cv.Optional(CONF_GAS_YEAR): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CUBIC_METER,
+            accuracy_decimals=3,
+            device_class=DEVICE_CLASS_GAS,
+            state_class=STATE_CLASS_TOTAL_INCREASING,
+        ),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def setup_sensor(config, key, setter):
+    """setting up sensor"""
+    if key not in config:
+        return None
+    var = await sensor.new_sensor(config[key])
+    cg.add(setter(var))
+    return var
+
+
+async def setup_input(config, key, setter):
+    """setting up input"""
+    if key not in config:
+        return None
+    var = await cg.get_variable(config[key])
+    cg.add(setter(var))
+    return var
+
+
+# code generation entry point
+async def to_code(config):
+    """Code generation entry point"""
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    await setup_input(config, CONF_TIME_ID, var.set_time)
+
+    # input sensors CHECK JBH
+    await setup_input(config, CONF_POWER, var.set_power)
+    await setup_input(config, CONF_TOTAL, var.set_total)
+
+    # exposed sensors
+    await setup_sensor(config, CONF_GAS_TODAY, var.set_gas_today)
+    await setup_sensor(config, CONF_GAS_YESTERDAY, var.set_gas_yesterday)
+    await setup_sensor(config, CONF_GAS_WEEK, var.set_gas_week)
+    await setup_sensor(config, CONF_GAS_MONTH, var.set_gas_month)
+    await setup_sensor(config, CONF_GAS_YEAR, var.set_gas_year)
