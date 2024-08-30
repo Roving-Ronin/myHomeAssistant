c45876fa402962efd4dd8da0c8184db3e0fa41e7
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-08-30 19:01:30 +1000

    Update const.py

--- components/gas_statistics_mj/const.py
+++ components/gas_statistics_mj/const.py
@@ -1,2 +1,8 @@
 # const.py
 UNIT_MEGAJOULE = "MJ"
+
+ICON_TODAY = "mdi:hours-24"
+ICON_YESTERDAY = "mdi:calendar-today-outline"
+ICON_WEEK = "mdi:calendar-week"
+ICON_MONTH = "mdi:calendar"
+ICON_YEAR = "mdi:calendar-text"
