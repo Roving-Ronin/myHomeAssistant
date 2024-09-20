abba27aad19163b6fb695205d2c464437d4d802f
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-09-20 22:24:34 +1000

    Update const.py

--- components/water_statistics/const.py
+++ components/water_statistics/const.py
@@ -1,5 +1,5 @@
 # const.py
-UNIT_LITRE = "L"
+#UNIT_LITRE = "L"  - No longer needed with ESPHome 2024.9.0 - https://github.com/esphome/esphome/pull/7317/files
 
 ICON_TODAY = "mdi:hours-24"
 ICON_YESTERDAY = "mdi:calendar-today-outline"
