f7dee775c1e93b7f237006e0443e7e0b343ab65b
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-09-27 20:43:05 +1000

    Update gas_statistics.cpp

--- components/gas_statistics/gas_statistics.cpp
+++ components/gas_statistics/gas_statistics.cpp
@@ -67,6 +67,7 @@ void GasStatistics::setup() {
   }
 }
 
+
 void GasStatistics::loop() {
   const auto t = this->time_->now();
   if (!t.is_valid()) {
@@ -90,15 +91,15 @@ void GasStatistics::loop() {
   this->gas_.start_today = total;
 
   if (this->gas_.current_day_of_year != 0) {
-    // start new week calculation
+    // at specified day of week we start a new week calculation
     if (t.day_of_week == this->gas_week_start_day_) {
       this->gas_.start_week = total;
     }
-    // start new month calculation
+    // at first day of month we start a new month calculation
     if (t.day_of_month == 1) {
       this->gas_.start_month = total;
     }
-    // start new year calculation
+    // at first day of year we start a new year calculation
     if (t.day_of_year == 1) {
       this->gas_.start_year = total;
     }
@@ -153,6 +154,9 @@ void GasStatistics::process_(float total) {
 
   // Save values to preferences
   this->save_();
+}
+
+void GasStatistics::save_() { this->pref_.save(&(this->gas_)); 
 }
 
 }  // namespace gas_statistics
