a0799c4102ca02ce85742a2d8686ec309238310d
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-09-27 19:37:35 +1000

    Update energy_statistics.h

--- components/energy_statistics/energy_statistics.h
+++ components/energy_statistics/energy_statistics.h
@@ -2,7 +2,6 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/preferences.h"
-#include "esphome/core/component_service.h"  // Include the necessary header for register_service
 
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/time/real_time_clock.h"
@@ -12,7 +11,7 @@ namespace energy_statistics {
 
 using sensor::Sensor;
 
-class EnergyStatistics : public Component, public ComponentService {  // Inherit from ComponentService
+class EnergyStatistics : public Component {  // Inherit only from Component
  public:
   // Set up priority to ensure this component is set up early
   float get_setup_priority() const override { return setup_priority::DATA; }
