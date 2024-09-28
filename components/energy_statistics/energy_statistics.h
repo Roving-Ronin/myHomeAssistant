02863e55d86109fd82bca14d478cdb04df6c28b6
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-09-28 14:52:55 +1000

    Update energy_statistics.h

--- components/energy_statistics/energy_statistics.h
+++ components/energy_statistics/energy_statistics.h
@@ -43,8 +43,11 @@ class EnergyStatistics : public Component {
   Sensor *energy_month_{nullptr};
   Sensor *energy_year_{nullptr};
 
-  // Resetting stored readings state flag
+  // Resetting state flag
   bool is_resetting_{false};
+
+  // To prevent sensor updates
+  bool prevent_sensor_update_{false}; // Add this line if you want to keep this functionality
 
   // start day of week configuration
   int energy_week_start_day_{2};
