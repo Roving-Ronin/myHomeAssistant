b9abcd1285e9efb3658bba9feb2e73f836303316
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-09-29 23:26:34 +1000

    Had gas_data_t instead of gas_data_mj_t

--- components/gas_statistics_mj/gas_statistics_mj.cpp
+++ components/gas_statistics_mj/gas_statistics_mj.cpp
@@ -34,9 +34,9 @@ void GasStatisticsMJ::dump_config() {
 }
 
 void GasStatisticsMJ::setup() {
-  this->pref_ = global_preferences->make_preference<gas_data_t>(fnv1_hash(TAG));
+  this->pref_ = global_preferences->make_preference<gas_data_mj_t>(fnv1_hash(TAG));
 
-  gas_data_t loaded{};
+  gas_data_mj_t loaded{};
   if (this->pref_.load(&loaded) && !this->is_resetting_) {
     this->gas_ = loaded;
 
