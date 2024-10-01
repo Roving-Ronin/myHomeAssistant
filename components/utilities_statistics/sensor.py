bceee36c2cfc427d77bbdfd08fbda2c39d980076
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-10-01 13:19:26 +1000

    Update sensor.py

--- components/utilities_statistics/sensor.py
+++ components/utilities_statistics/sensor.py
@@ -1,1 +1,36 @@
-// PLACE HOLDER
+# sensor.py
+
+from homeassistant.components.sensor import SensorEntity
+
+class UtilitiesSensor(SensorEntity):
+    """Representation of a sensor for the Utilities Component."""
+
+    def __init__(self, utilities_component, sensor_type):
+        """Initialize the sensor."""
+        self._utilities_component = utilities_component
+        self._sensor_type = sensor_type
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        if self._sensor_type == "gas_m3":
+            return "Gas Usage (m3)"
+        elif self._sensor_type == "gas_mj":
+            return "Gas Usage (MJ)"
+        elif self._sensor_type == "water":
+            return "Water Usage"
+    
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        if self._sensor_type == "gas_m3":
+            return self._utilities_component.data.gas_m3
+        elif self._sensor_type == "gas_mj":
+            return self._utilities_component.data.gas_mj
+        elif self._sensor_type == "water":
+            return self._utilities_component.data.water
+
+    def update(self):
+        """Fetch new state data for the sensor."""
+        self._utilities_component.loadFromNVS()  # Load latest values from NVS
