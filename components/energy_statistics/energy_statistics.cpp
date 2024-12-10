3e57547f7987d14222f6c5157526557107005e55
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-12-10 13:06:58 +1100

    L112-113 Added

--- components/energy_statistics/energy_statistics.cpp
+++ components/energy_statistics/energy_statistics.cpp
@@ -109,6 +109,8 @@ void EnergyStatistics::process_(float total) {
 }
 
 void EnergyStatistics::save_() { this->pref_.save(&(this->energy_)); }  // Save to flash memory
+  ESP_LOGD(TAG, "Energy Statistics - Values saved to flash memory (NVS)."); // Log message indicating save action
+}
 
 }  // namespace energy_statistics
 }  // namespace esphome
