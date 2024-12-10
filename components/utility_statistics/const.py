554de9453e7c75a1a231c84146fe00980cdf40aa
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2024-12-10 13:10:15 +1100

    Delete components/utility_statistics directory

--- components/utility_statistics/const.py
+++
@@ -1,9 +0,0 @@
-# const.py
-#UNIT_LITRE = "L"  - No longer needed with ESPHome 2024.9.0 - https://github.com/esphome/esphome/pull/7317/files
-UNIT_MEGAJOULE = "MJ"
-
-ICON_TODAY = "mdi:hours-24"
-ICON_YESTERDAY = "mdi:calendar-today-outline"
-ICON_WEEK = "mdi:calendar-week"
-ICON_MONTH = "mdi:calendar"
-ICON_YEAR = "mdi:calendar-text"
