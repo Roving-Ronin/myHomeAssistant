b23bb2a404e1b6ba5c212b847586fe88902e5e4f
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2025-01-23 13:34:31 +1100

    Update to merged Dentra Version
    Add partial time periods and year via official dentra route.

--- components/energy_statistics/energy_statistics.cpp
+++ components/energy_statistics/energy_statistics.cpp
@@ -6,36 +6,45 @@ namespace esphome {
 namespace energy_statistics {
 
 static const char *const TAG = "energy_statistics";
-static const char *const GAP = "  ";
+
+static const char *const PREF_V1 = "energy_statistics";
+static const char *const PREF_V2 = "energy_statistics_v2";
 
 void EnergyStatistics::dump_config() {
-  ESP_LOGCONFIG(TAG, "Energy Statistics - Sensors");
+  ESP_LOGCONFIG(TAG, "Energy statistics sensors");
   if (this->energy_today_) {
-    LOG_SENSOR(GAP, "Energy Today", this->energy_today_);
+    LOG_SENSOR("  ", "Energy Today", this->energy_today_);
   }
   if (this->energy_yesterday_) {
-    LOG_SENSOR(GAP, "Energy Yesterday", this->energy_yesterday_);
+    LOG_SENSOR("  ", "Energy Yesterday", this->energy_yesterday_);
   }
   if (this->energy_week_) {
-    LOG_SENSOR(GAP, "Energy Week", this->energy_week_);
+    LOG_SENSOR("  ", "Energy Week", this->energy_week_);
   }
   if (this->energy_month_) {
-    LOG_SENSOR(GAP, "Energy Month", this->energy_month_);
+    LOG_SENSOR("  ", "Energy Month", this->energy_month_);
   }
   if (this->energy_year_) {
-    LOG_SENSOR(GAP, "Energy Year", this->energy_year_);
+    LOG_SENSOR("  ", "Energy Year", this->energy_year_);
   }
 }
-
 
 void EnergyStatistics::setup() {
   this->total_->add_on_state_callback([this](float state) { this->process_(state); });
 
-  this->pref_ = global_preferences->make_preference<energy_data_t>(fnv1_hash(TAG));
-
-  energy_data_t loaded{};
-  if (this->pref_.load(&loaded)) {
-    this->energy_ = loaded;
+  this->pref_ = global_preferences->make_preference<energy_data_t>(fnv1_hash(PREF_V2));
+  bool loaded = this->pref_.load(&this->energy_);
+  if (!loaded) {
+    // migrating from v1 data
+    loaded = global_preferences->make_preference<energy_data_v1_t>(fnv1_hash(PREF_V1)).load(&this->energy_);
+    if (loaded) {
+      this->energy_.start_year = this->energy_.start_month;
+      // save as v2
+      this->pref_.save(&this->energy_);
+      global_preferences->sync();
+    }
+  }
+  if (loaded) {
     auto total = this->total_->get_state();
     if (!std::isnan(total)) {
       this->process_(total);
@@ -56,13 +65,14 @@ void EnergyStatistics::loop() {
     return;
   }
 
+  // update stats first time or on next day
   if (t.day_of_year == this->energy_.current_day_of_year) {
     // nothing to do
     return;
   }
 
-    // Save the current day's data
   this->energy_.start_yesterday = this->energy_.start_today;
+
   this->energy_.start_today = total;
 
   if (this->energy_.current_day_of_year != 0) {
@@ -77,7 +87,18 @@ void EnergyStatistics::loop() {
     // at first day of year we start a new year calculation
     if (t.day_of_year == 1) {
       this->energy_.start_year = total;
-    }
+    }    
+  }
+
+  // Intitialize all sensors. https://github.com/dentra/esphome-components/issues/65
+  if (this->energy_week_ && std::isnan(this->energy_.start_week)) {
+    this->energy_.start_week = this->energy_.start_yesterday;
+  }
+  if (this->energy_month_ && std::isnan(this->energy_.start_month)) {
+    this->energy_.start_month = this->energy_.start_yesterday;
+  }
+  if (this->energy_year_ && std::isnan(this->energy_.start_year)) {
+    this->energy_.start_year = this->energy_.start_yesterday;
   }
 
   this->energy_.current_day_of_year = t.day_of_year;
@@ -85,82 +106,29 @@ void EnergyStatistics::loop() {
   this->process_(total);
 }
 
-
-
 void EnergyStatistics::process_(float total) {
-  // Publish today's energy
   if (this->energy_today_ && !std::isnan(this->energy_.start_today)) {
     this->energy_today_->publish_state(total - this->energy_.start_today);
-  } else if (std::isnan(this->energy_.start_today)) {
-    // Initialize today's start value
-    this->energy_.start_today = total;
-    if (this->energy_today_) {
-      this->energy_today_->publish_state(0);  // Publish initial value as 0
-    }
   }
 
-  // Publish yesterday's energy
   if (this->energy_yesterday_ && !std::isnan(this->energy_.start_yesterday)) {
     this->energy_yesterday_->publish_state(this->energy_.start_today - this->energy_.start_yesterday);
   }
 
-  // Publish weekly energy (partial or full)
   if (this->energy_week_ && !std::isnan(this->energy_.start_week)) {
-    if (this->energy_.full_week_started) {
-      // Publish full calendar week value
-      this->energy_week_->publish_state(total - this->energy_.start_week);
-    } else {
-      // Publish partial week value
-      this->energy_week_->publish_state(total - this->energy_.start_week);
-    }
-  } else if (std::isnan(this->energy_.start_week)) {
-    // Initialize start_week for the first time
-    this->energy_.start_week = total;
-    if (this->energy_week_) {
-      this->energy_week_->publish_state(0);  // Publish initial value as 0
-    }
+    this->energy_week_->publish_state(total - this->energy_.start_week);
   }
 
-  // Publish monthly energy (partial or full)
   if (this->energy_month_ && !std::isnan(this->energy_.start_month)) {
-    if (this->energy_.full_month_started) {
-      // Publish full calendar month value
-      this->energy_month_->publish_state(total - this->energy_.start_month);
-    } else {
-      // Publish partial month value
-      this->energy_month_->publish_state(total - this->energy_.start_month);
-    }
-  } else if (std::isnan(this->energy_.start_month)) {
-    // Initialize start_month for the first time
-    this->energy_.start_month = total;
-    if (this->energy_month_) {
-      this->energy_month_->publish_state(0);  // Publish initial value as 0
-    }
+    this->energy_month_->publish_state(total - this->energy_.start_month);
   }
-
-  // Publish yearly energy (partial or full)
+  
   if (this->energy_year_ && !std::isnan(this->energy_.start_year)) {
-    if (this->energy_.full_year_started) {
-      // Publish full calendar year value
-      this->energy_year_->publish_state(total - this->energy_.start_year);
-    } else {
-      // Publish partial year value
-      this->energy_year_->publish_state(total - this->energy_.start_year);
-    }
-  } else if (std::isnan(this->energy_.start_year)) {
-    // Initialize start_year for the first time
-    this->energy_.start_year = total;
-    if (this->energy_year_) {
-      this->energy_year_->publish_state(0);  // Publish initial value as 0
-    }
+    this->energy_year_->publish_state(total - this->energy_.start_year);
   }
-
-  // Save the current state
-  this->save_();
+  
+  this->pref_.save(&this->energy_);
 }
-
-
-void EnergyStatistics::save_() { this->pref_.save(&(this->energy_)); }  // Save to flash memory
 
 }  // namespace energy_statistics
 }  // namespace esphome
