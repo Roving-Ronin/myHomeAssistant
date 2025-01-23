b343339db463dc72b2911eb2b88e70ab0af7d6ad
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2025-01-23 16:27:17 +1100

    Update to Dentra v2 Code

--- components/gas_statistics_mj/gas_statistics_mj.cpp
+++ components/gas_statistics_mj/gas_statistics_mj.cpp
@@ -6,41 +6,53 @@ namespace esphome {
 namespace gas_statistics_mj {
 
 static const char *const TAG = "gas_statistics_mj";
-static const char *const GAP = "  ";
+
+static const char *const PREF_V1 = "gas_statistics_mj";
+static const char *const PREF_V2 = "gas_statistics_mj_v2";
 
 void GasStatisticsMJ::dump_config() {
   ESP_LOGCONFIG(TAG, "Gas Statistics (MJ) - Sensors");
-    if (this->gas_today_ && !this->gas_today_->is_internal()) {
-    LOG_SENSOR(GAP, "Gas (MJ) Today", this->gas_today_);
+  if (this->gas_today_) {
+    LOG_SENSOR("  ", "Gas (MJ) Today", this->gas_today_);
   }
-  if (this->gas_yesterday_ && !this->gas_yesterday_->is_internal()) {
-    LOG_SENSOR(GAP, "Gas (MJ) Yesterday", this->gas_yesterday_);
+  if (this->gas_yesterday_) {
+    LOG_SENSOR("  ", "Gas (MJ) Yesterday", this->gas_yesterday_);
   }
-  if (this->gas_week_ && !this->gas_week_->is_internal()) {
-    LOG_SENSOR(GAP, "Gas (MJ) Week", this->gas_week_);
+  if (this->gas_week_) {
+    LOG_SENSOR("  ", "Gas (MJ) Week", this->gas_week_);
   }
-  if (this->gas_month_ && !this->gas_month_->is_internal()) {
-    LOG_SENSOR(GAP, "Gas (MJ) Month", this->gas_month_);
+  if (this->gas_month_) {
+    LOG_SENSOR("  ", "Gas (MJ) Month", this->gas_month_);
   }
-  if (this->gas_year_ && !this->gas_year_->is_internal()) {
-    LOG_SENSOR(GAP, "Gas (MJ) Year", this->gas_year_);
+  if (this->gas_year_) {
+    LOG_SENSOR("  ", "Gas (MJ) Year", this->gas_year_);
   }
 }
 
+
 void GasStatisticsMJ::setup() {
   this->total_->add_on_state_callback([this](float state) { this->process_(state); });
-  
-  this->pref_ = global_preferences->make_preference<gas_data_mj_t>(fnv1_hash(TAG));
 
-  gas_data_mj_t loaded{};
-  if (this->pref_.load(&loaded)) {
-    this->gas_ = loaded;
+  this->pref_ = global_preferences->make_preference<gas_mj_data_t>(fnv1_hash(PREF_V2));
+  bool loaded = this->pref_.load(&this->gas_);
+  if (!loaded) {
+    // migrating from v1 data
+    loaded = global_preferences->make_preference<gas_mj_data_v1_t>(fnv1_hash(PREF_V1)).load(&this->gas_);
+    if (loaded) {
+      this->gas_.start_year = this->gas_.start_month;
+      // save as v2
+      this->pref_.save(&this->gas_);
+      global_preferences->sync();
+    }
+  }
+  if (loaded) {
     auto total = this->total_->get_state();
     if (!std::isnan(total)) {
       this->process_(total);
     }
   }
 }
+
 
 void GasStatisticsMJ::loop() {
   const auto t = this->time_->now();
@@ -55,6 +67,7 @@ void GasStatisticsMJ::loop() {
     return;
   }
 
+  // update stats first time or on next day
   if (t.day_of_year == this->gas_.current_day_of_year) {
     // nothing to do
     return;
@@ -79,6 +92,17 @@ void GasStatisticsMJ::loop() {
     }
   }
 
+  // Intitialize all sensors. https://github.com/dentra/esphome-components/issues/65
+  if (this->gas_week_ && std::isnan(this->gas_.start_week)) {
+    this->gas_.start_week = this->gas_.start_yesterday;
+  }
+  if (this->gas_month_ && std::isnan(this->gas_.start_month)) {
+    this->gas_.start_month = this->gas_.start_yesterday;
+  }
+  if (this->gas_year_ && std::isnan(this->gas_.start_year)) {
+    this->gas_.start_year = this->gas_.start_yesterday;
+  }
+  
   this->gas_.current_day_of_year = t.day_of_year;
 
   this->process_(total);
@@ -86,78 +110,40 @@ void GasStatisticsMJ::loop() {
 
 
 void GasStatisticsMJ::process_(float total) {
-  // Publish today's gas
   if (this->gas_today_ && !std::isnan(this->gas_.start_today)) {
     this->gas_today_->publish_state(total - this->gas_.start_today);
-  } else if (std::isnan(this->gas_.start_today)) {
-    // Initialize today's start value
-    this->gas_.start_today = total;
-    if (this->gas_today_) {
-      this->gas_today_->publish_state(0);  // Publish initial value as 0
-    }
   }
 
-  // Publish yesterday's gas
   if (this->gas_yesterday_ && !std::isnan(this->gas_.start_yesterday)) {
     this->gas_yesterday_->publish_state(this->gas_.start_today - this->gas_.start_yesterday);
+  } else if (this->gas_yesterday_) {
+    // If there's no value for yesterday (NaN), publish 0
+    this->gas_yesterday_->publish_state(0);
   }
 
-  // Publish weekly gas (partial or full)
   if (this->gas_week_ && !std::isnan(this->gas_.start_week)) {
-    if (this->gas_.full_week_started) {
-      // Publish full calendar week value
-      this->gas_week_->publish_state(total - this->gas_.start_week);
-    } else {
-      // Publish partial week value
-      this->gas_week_->publish_state(total - this->gas_.start_week);
-    }
-  } else if (std::isnan(this->gas_.start_week)) {
-    // Initialize start_week for the first time
-    this->gas_.start_week = total;
-    if (this->gas_week_) {
-      this->gas_week_->publish_state(0);  // Publish initial value as 0
-    }
+    this->gas_week_->publish_state(total - this->gas_.start_week);
+  } else if (this->gas_week_) {
+    // If there's no value for week (NaN), publish 0
+    this->gas_week_->publish_state(0);
   }
 
-  // Publish monthly gas (partial or full)
   if (this->gas_month_ && !std::isnan(this->gas_.start_month)) {
-    if (this->gas_.full_month_started) {
-      // Publish full calendar month value
-      this->gas_month_->publish_state(total - this->gas_.start_month);
-    } else {
-      // Publish partial month value
-      this->gas_month_->publish_state(total - this->gas_.start_month);
-    }
-  } else if (std::isnan(this->gas_.start_month)) {
-    // Initialize start_month for the first time
-    this->gas_.start_month = total;
-    if (this->gas_month_) {
-      this->gas_month_->publish_state(0);  // Publish initial value as 0
-    }
+    this->gas_month_->publish_state(total - this->gas_.start_month);
+  } else if (this->gas_month_) {
+    // If there's no value for month (NaN), publish 0
+    this->gas_month_->publish_state(0);
   }
 
-  // Publish yearly gas (partial or full)
   if (this->gas_year_ && !std::isnan(this->gas_.start_year)) {
-    if (this->gas_.full_year_started) {
-      // Publish full calendar year value
-      this->gas_year_->publish_state(total - this->gas_.start_year);
-    } else {
-      // Publish partial year value
-      this->gas_year_->publish_state(total - this->gas_.start_year);
-    }
-  } else if (std::isnan(this->gas_.start_year)) {
-    // Initialize start_year for the first time
-    this->gas_.start_year = total;
-    if (this->gas_year_) {
-      this->gas_year_->publish_state(0);  // Publish initial value as 0
-    }
+    this->gas_year_->publish_state(total - this->gas_.start_year);
+  } else if (this->gas_year_) {
+    // If there's no value for year (NaN), publish 0
+    this->gas_year_->publish_state(0);
   }
 
-  // Save the current state
-  this->save_();
+  this->pref_.save(&this->gas_);
 }
-
-void GasStatisticsMJ::save_() { this->pref_.save(&this->gas_); } // Save to flash memory
 
 }  // namespace gas_statistics
 }  // namespace esphome
