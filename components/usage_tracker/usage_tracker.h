02d055bce3d64605e19120984dd9ea77e0a201fd
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2025-04-22 18:04:45 +1000

    Update usage_tracker.h

--- components/usage_tracker/usage_tracker.h
+++ components/usage_tracker/usage_tracker.h
@@ -1,16 +1,18 @@
 #pragma once
+
 #include "esphome/core/component.h"
+#include "esphome/core/preferences.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome {
-namespace usage_timer {
+namespace usage_tracker {
 
-class UsageTimer : public Component {
+class UsageTracker : public Component {
  public:
   void set_sensor(binary_sensor::BinarySensor *sensor) { this->sensor_ = sensor; }
-  void set_last_on_duration_sensor(sensor::Sensor *s) { this->last_on_duration_sensor_ = s; }
-  void set_lifetime_use_sensor(sensor::Sensor *s) { this->lifetime_use_sensor_ = s; }
+  void set_last_on_duration_sensor(sensor::Sensor *sensor) { this->last_on_duration_sensor_ = sensor; }
+  void set_lifetime_use_sensor(sensor::Sensor *sensor) { this->lifetime_use_sensor_ = sensor; }
 
   void setup() override;
   void loop() override;
@@ -20,10 +22,12 @@ class UsageTimer : public Component {
   sensor::Sensor *last_on_duration_sensor_{nullptr};
   sensor::Sensor *lifetime_use_sensor_{nullptr};
 
-  bool was_on_ = false;
-  uint32_t on_start_time_ = 0;
-  float total_lifetime_use_sec_ = 0;
+  bool was_on_{false};
+  uint32_t on_start_time_{0};
+  float total_lifetime_use_{0};
+
+  ESPPreferenceObject lifetime_use_pref_;
 };
 
-}  // namespace usage_timer
+}  // namespace usage_tracker
 }  // namespace esphome
