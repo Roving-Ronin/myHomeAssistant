f9457f0e1d9ba93285a0d6a9c06f061884b07ea0
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2025-04-22 18:25:04 +1000

    Update usage_tracker.h

--- components/usage_tracker/usage_tracker.h
+++ components/usage_tracker/usage_tracker.h
@@ -12,22 +12,28 @@ using sensor::Sensor;
 
 class UsageTracker : public Component {
  public:
-  void set_sensor(binary_sensor::BinarySensor *sensor) { this->sensor_ = sensor; }
-  void set_last_on_duration_sensor(Sensor *sensor) { this->last_on_duration_sensor_ = sensor; }
-  void set_lifetime_use_sensor(Sensor *sensor) { this->lifetime_use_sensor_ = sensor; }
-
+  // Setup and loop
   void setup() override;
   void loop() override;
+  void dump_config() override;
+
+  // Setters
+  void set_sensor(binary_sensor::BinarySensor *sensor) { this->sensor_ = sensor; }
+  void set_last_on_duration_sensor(sensor::Sensor *sensor) { this->last_on_duration_sensor_ = sensor; }
+  void set_lifetime_use_sensor(sensor::Sensor *sensor) { this->lifetime_use_sensor_ = sensor; }
 
  protected:
+  // Sensors
   binary_sensor::BinarySensor *sensor_{nullptr};
-  Sensor *last_on_duration_sensor_{nullptr};
-  Sensor *lifetime_use_sensor_{nullptr};
+  sensor::Sensor *last_on_duration_sensor_{nullptr};
+  sensor::Sensor *lifetime_use_sensor_{nullptr};
 
+  // Internal state
   bool was_on_{false};
   uint32_t on_start_time_{0};
   float total_lifetime_use_{0};
 
+  // NVS storage
   ESPPreferenceObject lifetime_use_pref_;
   uint32_t last_save_time_{0};
 };
