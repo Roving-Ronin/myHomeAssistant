c367edf51c550a5e6b3ca095e5c0e3f344d069e1
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2025-04-22 18:14:18 +1000

    Update usage_tracker.h

--- components/usage_tracker/usage_tracker.h
+++ components/usage_tracker/usage_tracker.h
@@ -8,19 +8,21 @@
 namespace esphome {
 namespace usage_tracker {
 
+using sensor::Sensor;
+
 class UsageTracker : public Component {
  public:
   void set_sensor(binary_sensor::BinarySensor *sensor) { this->sensor_ = sensor; }
-  void set_last_on_duration_sensor(sensor::Sensor *sensor) { this->last_on_duration_sensor_ = sensor; }
-  void set_lifetime_use_sensor(sensor::Sensor *sensor) { this->lifetime_use_sensor_ = sensor; }
+  void set_last_on_duration_sensor(Sensor *sensor) { this->last_on_duration_sensor_ = sensor; }
+  void set_lifetime_use_sensor(Sensor *sensor) { this->lifetime_use_sensor_ = sensor; }
 
   void setup() override;
   void loop() override;
 
  protected:
   binary_sensor::BinarySensor *sensor_{nullptr};
-  sensor::Sensor *last_on_duration_sensor_{nullptr};
-  sensor::Sensor *lifetime_use_sensor_{nullptr};
+  Sensor *last_on_duration_sensor_{nullptr};
+  Sensor *lifetime_use_sensor_{nullptr};
 
   bool was_on_{false};
   uint32_t on_start_time_{0};
