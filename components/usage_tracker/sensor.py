e2f154ca3d4c58cb7ff183b549119cd69b2fc58c
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2025-04-22 17:54:28 +1000

    Update sensor.py

--- components/usage_tracker/sensor.py
+++ components/usage_tracker/sensor.py
@@ -1,25 +1,46 @@
-from esphome import automation
 import esphome.config_validation as cv
 import esphome.codegen as cg
+from esphome import automation
 from esphome.components import sensor, binary_sensor
+from esphome.components import sensor, time
 from esphome.const import CONF_ID, CONF_SENSOR
 
+# Import your custom ICONS from const.py
+from .const import (
+    DOMAIN,
+    ICON_LAST_USE,
+    ICON_LIFETIME_USE,
+)
+
 CODEOWNERS = ["@roving-ronin"]
-DEPENDENCIES = []
+DEPENDENCIES = ["time"]
 
 CONF_ON_OFF_SENSOR = "on_off_sensor"
 CONF_LAST_ON_DURATION = "last_on_duration"
 CONF_LIFETIME_USE = "lifetime_use"
 
-usage_timer_ns = cg.esphome_ns.namespace("usage_timer")
-UsageTimer = usage_timer_ns.class_("UsageTimer", cg.Component)
+usage_tracker_ns = cg.esphome_ns.namespace("usage_tracker")
+UsageTracker = usage_tracker_ns.class_("UsageTracker", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(UsageTimer),
+        cv.GenerateID(): cv.declare_id(UsageTracker),
+        cv.GenerateID(CONF_TIME_ID): cv.use_id(time.RealTimeClock),
         cv.Required(CONF_ON_OFF_SENSOR): cv.use_id(binary_sensor.BinarySensor),
-        cv.Required(CONF_LAST_ON_DURATION): sensor.sensor_schema(),
-        cv.Required(CONF_LIFETIME_USE): sensor.sensor_schema(),
+        cv.Required(CONF_LAST_ON_DURATION): sensor.sensor_schema(
+            unit_of_measurement=SECONDS,
+            icon=ICON_LAST_USE,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_DURATION,
+            state_class=STATE_CLASS_TOTAL_INCREASING,
+        ),
+        cv.Required(CONF_LIFETIME_USE): sensor.sensor_schema(
+            unit_of_measurement=SECONDS,
+            icon=ICON_LIFETIME_USE,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_DURATION,
+            state_class=STATE_CLASS_TOTAL_INCREASING,
+        ),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
