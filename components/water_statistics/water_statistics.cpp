50eecb2938c80faa1aac3718c9b9bce999987c3f
Roving Ronin <108674933+Roving-Ronin@users.noreply.github.com>
2025-05-01 14:22:56 +1000

    Update water_statistics.cpp

--- components/water_statistics/water_statistics.cpp
+++ components/water_statistics/water_statistics.cpp
@@ -57,7 +57,7 @@ void WaterStatistics::setup() {
   }
 
   // Delay initial loop processing until time sync
-  this->set_timeout(10000, [this]() { this->initial_processing_started_ = true; });
+  this->set_timeout(15000, [this]() { this->initial_processing_started_ = true; });
 }
 
 void WaterStatistics::on_shutdown() {
